80b0d47d10648553d8b5fcd3b9a48ed0e419c03e
Jonathan <djcors@MacBook-Pro-de-Jonathan.local>
2015-05-19 21:04:50 -0500

    Initial Commit

---
+++ webservice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webservice
+//
+//  Created by Jonathan on 19/05/15.
+//  Copyright (c) 2015 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
